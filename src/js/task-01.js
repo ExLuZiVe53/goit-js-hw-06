be3b50c1ed159e135b99ef070d88cbf5c4d10819
ExLuZiVe53 <imstudent27@gmail.com>
2023-07-05 11:36:57 +0200

    accsess task 1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,1 +1,11 @@
-const categories = document.querySelector;
+const categories = document.querySelector('#categories');
+
+console.log(
+  `Number of categories ${categories.querySelectorAll('.item').length}`
+);
+const variable = categories.children;
+
+[...variable].forEach(li => {
+  console.log(`Category : ${li.querySelector('h2').textContent}`);
+  console.log(`Elements : ${li.querySelectorAll('li').length}`);
+});
