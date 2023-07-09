c4b954296bb826a7195395bc403f6221aa7391e9
ExLuZiVe53 <imstudent27@gmail.com>
2023-07-09 17:24:00 +0200

    Update task-04.js

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,14 +1,2 @@
-const CounterInput = function ({
-  rootSelector,
-  initialValue = 0,
-  step = 1,
-} = {}) {
-  this._value = initialValue;
-  this._step = step;
-
-  this._refs = this._getRefs(rootSelector);
-};
-CounterInput.prototype._getRefs = function (rootSelector) {
-  const refs = {};
-  refs.container = document.querySelector(#counter);
-};
+const containerValue = document.querySelector('#counter');
+console.log(containerValue);
