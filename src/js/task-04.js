1769565474c521263e1ee87efc73db02569e284f
ExLuZiVe53 <imstudent27@gmail.com>
2023-07-11 22:00:17 +0200

    update task 4

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,18 +1,3 @@
-// const containerValue = document.querySelector('#counter');
-
-// const btnDown = document.querySelector('button[data-action="decrement"]');
-// const btnUp = document.querySelector('button[data-action="increment"]');
-
-// btnDown.addEventListener('click', () => {
-//   console.log('Button Down was clicked');
-// });
-
-// btnUp.addEventListener('click', () => {
-//   console.log('Button Up was clicked');
-// });
-
-// console.dir(btnDown);
-
 const CounterPlugin = function ({
   rootSelector,
   counterValue = 0,
