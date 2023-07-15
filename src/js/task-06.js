3c195a4a07cd6c7f75f1ea46ad99fcd792271ed7
ExLuZiVe53 <imstudent27@gmail.com>
2023-07-15 09:53:03 +0200

    Start task 6

---
+++ src/js/task-06.js
@@ -0,0 +1,14 @@
+const inputChange = document.getElementById('validation-input');
+
+form.elements.pass.addEventListener('blur', onBlur);
+
+function onBlur(e) {
+  const length = e.target.value.length;
+  if (length >= 6 && length <= 15) {
+    inputChange.classList.add('#validation-input.valid');
+    inputChange.classList.remove('#validation-input.invalid');
+  } else {
+    inputChange.classList.add('#validation-input.invalid');
+    inputChange.classList.remove('#validation-input.valid');
+  }
+}
