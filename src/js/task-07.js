c15f97323fc70f5b65c9cf18f6cea9b8e3b69fc1
ExLuZiVe53 <imstudent27@gmail.com>
2023-07-16 20:28:35 +0200

    add task 9

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,1 +1,22 @@
-const reloadInput = document.querySelector('#font-size-control');
+const refs = {
+  reloadInput: document.querySelector('#font-size-control'),
+  textEl: document.querySelector('#text'),
+};
+
+let size = refs.reloadInput.value;
+let fontSize = refs.textEl.style.fontSize;
+console.dir(size);
+
+refs.reloadInput.addEventListener('input', resizeInputSizeIn);
+refs.textEl.addEventListener('input', resizeInputSizeOut);
+console.log(refs.textEl);
+
+function resizeInputSizeIn(e) {
+  size += 1;
+  refs.textEl.style.cssText = `fontSize: ${size}`;
+}
+
+function resizeInputSizeOut(e) {
+  fontSize -= 1;
+  refs.textEl.style.cssText = `fontSize: ${fontSize}`;
+}
