ecf05a671f7844f53f9a2c6616da94a67fab2dfb
ExLuZiVe53 <imstudent27@gmail.com>
2023-07-17 22:14:10 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,14 +1,16 @@
 const formEl = document.querySelector('.login-form');
-
-const email = formEl.email.value;
-console.log(email);
-const password = formEl.password.value;
+// const { email, password } = evt.target.elements;
 
 formEl.addEventListener('submit', onSubmit);
 
 function onSubmit(evt) {
   preventDefalt(evt);
+  const email = formEl.elements.email.value.trim();
+  console.log(email);
+  const password = formEl.elements.password.value;
   if (email === '' || password === '') {
     alert('Всі поля повинні бути заповнені!');
   }
 }
+
+// const userData = { name: email.value, password: password.value };
