838f2d5423f1b4cc30303e6a25a7580e8a963516
ExLuZiVe53 <imstudent27@gmail.com>
2023-07-18 14:51:31 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,16 +1,31 @@
+// const formEl = document.querySelector('.login-form');
+// // const { email, password } = evt.target.elements;
+
+// formEl.addEventListener('submit', onSubmit);
+
+// function onSubmit(evt) {
+//   evt.preventDefalt();
+//   const email = formEl.elements.email.value.trim();
+//   console.log(email);
+//   const password = formEl.elements.password.value;
+//   if (email === '' || password === '') {
+//     alert('Всі поля повинні бути заповнені!');
+//   }
+// }
+
+// const userData = { name: email.value, password: password.value };
+
 const formEl = document.querySelector('.login-form');
-// const { email, password } = evt.target.elements;
 
-formEl.addEventListener('submit', onSubmit);
+formEl.addEventListener('submit', event => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
 
-function onSubmit(evt) {
-  preventDefalt(evt);
-  const email = formEl.elements.email.value.trim();
-  console.log(email);
-  const password = formEl.elements.password.value;
   if (email === '' || password === '') {
     alert('Всі поля повинні бути заповнені!');
   }
-}
-
-// const userData = { name: email.value, password: password.value };
+  console.log(`Email : ${email.value}`);
+  console.log(`Password : ${password.value}`);
+});
