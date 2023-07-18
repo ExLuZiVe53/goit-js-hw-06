5808dbb5424badea340779713d637e7e3e2cba9f
ExLuZiVe53 <imstudent27@gmail.com>
2023-07-18 14:21:17 +0200

    reload task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,15 @@
+// const categories = document.querySelector('#categories');
+
+// console.log(
+//   `Number of categories ${categories.querySelectorAll('.item').length}`
+// );
+// const variable = categories.children;
+
+// [...variable].forEach(li => {
+//   console.log(`Category : ${li.querySelector('h2').textContent}`);
+//   console.log(`Elements : ${li.querySelectorAll('li').length}`);
+// });
+
 const categories = document.querySelector('#categories');
 
 console.log(
@@ -5,7 +17,7 @@ console.log(
 );
 const variable = categories.children;
 
-[...variable].forEach(li => {
-  console.log(`Category : ${li.querySelector('h2').textContent}`);
-  console.log(`Elements : ${li.querySelectorAll('li').length}`);
+[...variable].forEach(el => {
+  console.log(`Category: ${el.firstElementChild.textContent}`);
+  console.log(`Elements: ${el.lastElementChild.children.length}`);
 });
